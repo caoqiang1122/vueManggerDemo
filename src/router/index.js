2f6d391a6a3e99860bc9778bc7b488916936b207
caoqiang <caoqiang@csii.com.cn>
2019-01-21 15:44:20 +0800

    主页面和注册页面的添加

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import home from '../view/layout/Main/home.vue'
 import login from '../components/login'
 import basetable from '../components/basetable'
 import chart from '../components/charts'
+import mainactive from '../components/mainActive'
+import register from '../components/register'
 
 Vue.use(Router)
 
@@ -20,6 +22,14 @@ export default new Router({
         {
           path: '/home/charts',
           component: chart
+        },
+        {
+          path: '/home/mainActive',
+          component: mainactive
+        },
+        {
+          path: '/home/register',
+          component: register
         }
       ]
     },
